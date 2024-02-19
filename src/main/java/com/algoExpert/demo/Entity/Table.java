af33b3f36389d0603c2effa141ae1d8458837dd2
mbali92 <83025186+mbali92@users.noreply.github.com>
2024-02-19 11:10:15 +0200

    add: added three fields id, tablename - to hold the table name, Task - list of task with one to many relationship.

--- src/main/java/com/algoExpert/demo/Entity/Table.java
+++ src/main/java/com/algoExpert/demo/Entity/Table.java
@@ -1,3 +1,17 @@
+package com.algoExpert.demo.Entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
 public class Table {
+    @Id
+    @GeneratedValue
+    private int table_id;
+    private String table_name;
+
+    @OneToMany
+    List<Task> tasks;
 
 }
