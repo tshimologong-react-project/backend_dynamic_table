acc11345c3de4124650700304d527997d7a8a896
TerrenceJ <siyabonga@example.com>
2024-03-08 12:05:51 +0200

    feat: created a members entity along with its repository, service and controller

--- src/main/java/com/algoExpert/demo/Controller/MemberController.java
+++ src/main/java/com/algoExpert/demo/Controller/MemberController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MemberController {
-
     @Autowired
     private MemberService memberService;
 
--- src/main/java/com/algoExpert/demo/Service/MemberService.java
+++ src/main/java/com/algoExpert/demo/Service/MemberService.java
@@ -12,7 +12,6 @@ import java.util.List;
 
 @Service
 public class MemberService {
-
     @Autowired
     private ProjectRepository projectRepository;
 
